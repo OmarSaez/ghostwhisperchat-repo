acda33428f6228bc27c54e41a81ef2ba6512bf97
OmarSaez <omar.saez@usach.cl>
2025-12-15 22:51:48 -0300

    Se transformo en un servicio v42.0

--- ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/gw_shared.py
+++ ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/gw_shared.py
@@ -9,8 +9,8 @@ import time
 import hashlib
 
 # v38.0: Shared Definitions for Modular Architecture
-APP_VER_NUM = 41.25
-APP_VER_TAG = "Robust Priv Disc"
+APP_VER_NUM = 42.00
+APP_VER_TAG = "Ahora es un servicio!"
 APP_VERSION = f"v{APP_VER_NUM} ({APP_VER_TAG})"
 
 # --- CONFIG POP ANTI-SPAM ---
