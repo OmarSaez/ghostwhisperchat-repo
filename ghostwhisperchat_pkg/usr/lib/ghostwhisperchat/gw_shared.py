2cc16c5f254d1caa206348f9beccf9b58943059b
OmarSaez <omar.saez@usach.cl>
2025-12-16 18:21:47 -0300

    GhostWhisperChat v42.25 - VERBOSE INTEGRITY TEST

--- ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/gw_shared.py
+++ ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/gw_shared.py
@@ -9,8 +9,8 @@ import time
 import hashlib
 
 # v38.0: Shared Definitions for Modular Architecture
-APP_VER_NUM = 42.24
-APP_VER_TAG = "Fix: Lanzamiento Robusto de Hijos (Shell Wrapper)"
+APP_VER_NUM = 42.25
+APP_VER_TAG = "Feat: Test Integridad Mejorado (Verbose Test)"
 APP_VERSION = f"v{APP_VER_NUM} ({APP_VER_TAG})"
 
 # --- CONFIG POP ANTI-SPAM ---
