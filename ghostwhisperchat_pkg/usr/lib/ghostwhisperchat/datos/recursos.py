f405b4050779a242fd37232750bcbdd7fcaca7ed
OmarSaez <omar.saez@usach.cl>
2025-12-20 13:02:00 -0300

    Correcion en como lanzar el demonio

--- ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
+++ ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
@@ -22,8 +22,8 @@ class Colores:
     BG_RED = "\033[41m"
 
 # Versionado
-APP_VER_NUM = "2.4"
-APP_VER_TAG = "Corrección de ruta del cliente"
+APP_VER_NUM = "2.5"
+APP_VER_TAG = "Systemd Persistence Fix"
 APP_VERSION = f"v{APP_VER_NUM} ({APP_VER_TAG})"
 
 BANNER = r"""
