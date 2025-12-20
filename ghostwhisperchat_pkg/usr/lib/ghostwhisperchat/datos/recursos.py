31f317ced789ef849fc4ea27b2cfa5e3db2e0af7
OmarSaez <omar.saez@usach.cl>
2025-12-20 12:27:18 -0300

    Nuevo v2.2 apt

--- ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
+++ ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
@@ -22,8 +22,8 @@ class Colores:
     BG_RED = "\033[41m"
 
 # Versionado
-APP_VER_NUM = "2.1"
-APP_VER_TAG = "Corrección de rutas"
+APP_VER_NUM = "2.2"
+APP_VER_TAG = "Corrección de empaquetado"
 APP_VERSION = f"v{APP_VER_NUM} ({APP_VER_TAG})"
 
 BANNER = r"""
