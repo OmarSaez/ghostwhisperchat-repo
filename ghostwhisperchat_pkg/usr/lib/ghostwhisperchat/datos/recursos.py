1d40d6ce36fefaddb418388f5b62018c28f6a852
OmarSaez <omar.saez@usach.cl>
2025-12-20 17:19:06 -0300

    Version v2.28 apt

--- ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
+++ ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
@@ -22,8 +22,8 @@ class Colores:
     BG_RED = "\033[41m"
 
 # Versionado
-APP_VER_NUM = "2.27"
-APP_VER_TAG = "UX Polish"
+APP_VER_NUM = "2.28"
+APP_VER_TAG = "Comandos Transitorios"
 APP_VERSION = f"v{APP_VER_NUM} ({APP_VER_TAG})"
 
 BANNER = r"""
