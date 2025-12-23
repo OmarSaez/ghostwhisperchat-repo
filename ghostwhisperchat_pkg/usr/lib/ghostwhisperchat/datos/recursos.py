c8103233f29b87c8d2a3b354703e4a853e0e07a3
OmarSaez <omar.saez@usach.cl>
2025-12-23 15:56:54 -0300

    Subiendo version apt estable

--- ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
+++ ghostwhisperchat_pkg/usr/lib/ghostwhisperchat/datos/recursos.py
@@ -27,8 +27,8 @@ class Colores:
     BG_RED = "\033[41m"
 
 # Versionado
-APP_VER_NUM = "2.100"
-APP_VER_TAG = "Diagnóstico Universal y Puertos"
+APP_VER_NUM = "2.110"
+APP_VER_TAG = "Chat LAN estable"
 APP_VERSION = f"v{APP_VER_NUM} ({APP_VER_TAG})"
 
 BANNER = r"""
